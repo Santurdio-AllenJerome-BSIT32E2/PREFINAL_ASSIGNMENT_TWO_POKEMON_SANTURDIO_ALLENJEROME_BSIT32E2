eaa47dae284038a51af92af0aedbab84b9e8f264
Santurdio-AllenJerome-BSIT32E2 <145907858+Santurdio-AllenJerome-BSIT32E2@users.noreply.github.com>
2024-06-12 19:33:05 +0800

    update

---
+++ PokemonApp_Santurdio_Allen_Jerome/PokemonApp/Controllers/PokemonController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PokemonApp.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PokemonApp.Controllers
+{
+    public class PokemonController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public PokemonController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var pokemon = await GetPokemon("treecko");
+            return View(pokemon);
+        }
+
+        private async Task<Pokemon> GetPokemon(string name)
+        {
+            var response = await _httpClient.GetStringAsync($"https://pokeapi.co/api/v2/pokemon/{name}");
+            return JsonConvert.DeserializeObject<Pokemon>(response);
+        }
+    }
+}
---
+++ PokemonApp_Santurdio_Allen_Jerome/PokemonApp/Models/Pokemon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace PokemonApp.Models
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public List<PokemonAbility> Abilities { get; set; }
+        public List<PokemonMove> Moves { get; set; }
+
+        public class PokemonAbility
+        {
+            public AbilityDetail Ability { get; set; }
+
+            public class AbilityDetail
+            {
+                public string Name { get; set; }
+            }
+        }
+
+        public class PokemonMove
+        {
+            public MoveDetail Move { get; set; }
+
+            public class MoveDetail
+            {
+                public string Name { get; set; }
+            }
+        }
+    }
+}
